046a054110ef7c1e2fd1558ab0f7dea6017c72a6
Vinay Raj Choudhary <vinayrajchoudhary@gmail.com>
2026-09-19 17:14:58 +0530

    Refresh cache for seamless transition

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "kndle-scent-game-v10";
+const CACHE = "kndle-scent-game-v11";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,7 +7,7 @@ const ASSETS = [
   "./db.js",
   "./cloud.js?v=6",
   "./transition.js",
-  "./app.js?v=10",
+  "./app.js?v=11",
   "./manifest.webmanifest"
 ];
 
